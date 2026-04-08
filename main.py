796e0b01cffc12e5136908eb53b44ae1c88231e5
julia-b-c <12227126@zju.edu.cn>
2026-04-08 23:58:24 +0800

    Fix web startup entrypoint

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import sys
 from game_v3_ios import run_game, run_game_web
 
 
-if __name__ == "__main__":
-    if sys.platform == "emscripten":
-        asyncio.run(run_game_web())
-    else:
-        run_game()
+if sys.platform == "emscripten":
+    asyncio.create_task(run_game_web())
+else:
+    run_game()
